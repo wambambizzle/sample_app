bcaf38ba16e0e77cb3ae1eddd1f0d787cde267e6
Jordan Anderson <jordan.anderson@izea.com>
2016-07-15 14:22:54 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,7 @@ describe "StaticPages" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title(" #{base_title} ") }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
@@ -34,4 +34,11 @@ describe "StaticPages" do
     it { should have_content('Help') }
     it { should have_title(full_title('Help')) }
   end
+
+  # describe "signup page" do
+  #   before { visit signup_path }
+  #
+  #   it { should have_content('Sign up') }
+  #   it { should have_title(full_title('Sign up')) }
+  # end
 end
